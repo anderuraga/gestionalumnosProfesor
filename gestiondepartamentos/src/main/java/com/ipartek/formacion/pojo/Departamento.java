3c9ebb7ae2c01fb1fc9a5ba9f6d0c28467703e5d
TriquiTrueno <erasmo.seebold@gmail.com>
2016-07-27 10:29:15 +0200

    Clases Empleado.java y Departamento.java creadas

---
+++ gestiondepartamentos/src/main/java/com/ipartek/formacion/pojo/Departamento.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion.pojo;
+
+public class Departamento {
+
+	public static final int CODIGO_ALUMNO = -1;
+	private int codigo;
+	private String nombre;
+	private String descripcion;
+
+}
---
+++ gestiondepartamentos/src/main/java/com/ipartek/formacion/pojo/Empleado.java
@@ -0,0 +1,20 @@
+package com.ipartek.formacion.pojo;
+
+import java.util.Date;
+
+public class Empleado {
+
+	public static final int CODIGO_ALUMNO = -1;
+	private int codigo;
+	private String nombre;
+	private String apellidos;
+	private Date date_nacimiento;
+	private Date date_contratacion;
+	private String num_seg_soc;
+	private String num_dni;
+	private String cuenta_corriente;
+	private String direccion;
+	private String localidad;
+	private String codigo_postal;
+	
+}
