6f38e59f2eaedcdad4fe4c34748c883ff4458482
josuasua <josuasua@hotmail.com>
2016-07-27 09:10:43 +0200

    gestionempleados, no rompas nada

---
+++ gestionempleados/src/main/java/org/formacion/ipartek/gestionempleados/App.java
@@ -0,0 +1,13 @@
+package org.formacion.ipartek.gestionempleados;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
