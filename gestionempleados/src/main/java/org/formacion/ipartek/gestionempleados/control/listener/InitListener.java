23caeb54fa21bd20bcf4a0bc1cf794d2e56f924e
josuasua <josuasua@hotmail.com>
2016-07-27 10:11:24 +0200

    DepartamentoDAO

---
+++ gestionempleados/src/main/java/org/formacion/ipartek/gestionempleados/control/listener/InitListener.java
@@ -0,0 +1,98 @@
+package org.formacion.ipartek.gestionempleados.control.listener;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * Application Lifecycle Listener implementation class InitListener
+ *
+ */
+public class InitListener implements ServletContextListener, ServletContextAttributeListener {
+
+	private final static String PROPS_NAME = "properties";
+	private final static Logger LOG = Logger.getLogger(InitListener.class);
+	private final static String PATH_LOG4J = "WEB-INF/conf/log4j.properties";
+
+	/**
+	 * Default constructor.
+	 */
+	public InitListener() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+	 */
+	@Override
+	public void attributeAdded(ServletContextAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
+	 */
+	@Override
+	public void attributeRemoved(ServletContextAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+	 */
+	@Override
+	public void attributeReplaced(ServletContextAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
+	 */
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		loadLog4j(arg0);
+		loadProperties(arg0);
+	}
+
+	private void loadProperties(ServletContextEvent arg0) {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream input = classLoader.getResourceAsStream("constantes.properties");
+		Properties props = new Properties();
+		try {
+			props.load(input);
+			LOG.info("Se ha cargado correctamente las constantes desde el properties");
+		} catch (IOException e) {
+			LOG.error("No se ha cargado correctamente las constantes desde el properties");
+		}
+		arg0.getServletContext().setAttribute(PROPS_NAME, props);
+	}
+
+	private void loadLog4j(ServletContextEvent arg0) {
+		String ruta = arg0.getServletContext().getRealPath("/");
+
+		try {
+			PropertyConfigurator.configure(ruta + PATH_LOG4J);
+			LOG.info("Log cargado");
+
+		} catch (Exception e) {
+
+		}
+	}
+
+}
---
+++ gestionempleados/src/main/java/org/formacion/ipartek/gestionempleados/dbms/DAO/DepartamentoDAO.java
@@ -0,0 +1,11 @@
+package org.formacion.ipartek.gestionempleados.dbms.DAO;
+
+import java.util.List;
+
+import org.formacion.ipartek.gestionempleados.pojo.Empleado;
+
+public interface DepartamentoDAO {
+
+	public List<Empleado> getAll();
+
+}
