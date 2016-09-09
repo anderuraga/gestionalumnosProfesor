6409e50ef17cafc3f5c97a72b09951759f67b0c9
Imanol <imanol.8693@gmail.com>
2016-09-09 13:32:54 +0200

    Validacion Phone & i18n añadido

--- gestionalumnosIJ/src/main/java/com/ipartek/formacion/dao/persistence/Phone.java
+++ gestionalumnosIJ/src/main/java/com/ipartek/formacion/dao/persistence/Phone.java
@@ -6,10 +6,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.validation.Constraint;
 import javax.validation.Payload;
 
+import com.ipartek.formacion.controlador.validator.PhoneValidator;
+
 @Documented
-// @Constraint(validatedBy)
+@Constraint(validatedBy = PhoneValidator.class)
 @Target({ ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Phone {
