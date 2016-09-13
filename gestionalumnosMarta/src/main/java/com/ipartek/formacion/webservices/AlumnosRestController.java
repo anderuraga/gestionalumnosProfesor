0c2a829ec8c103e80be9266418cbccb02b4c928b
Curso <Curso@Formacion-TOS12.ipartekaula.com>
2016-09-13 13:10:53 +0200

    AlumosRestControler

--- gestionalumnosMarta/src/main/java/com/ipartek/formacion/webservices/AlumnosRestController.java
+++ gestionalumnosMarta/src/main/java/com/ipartek/formacion/webservices/AlumnosRestController.java
@@ -3,7 +3,11 @@ package com.ipartek.formacion.webservices;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +23,72 @@ public class AlumnosRestController {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<Alumno>> getAll(){ //ResponseEntity->elemento de spring que encapsula los encabezados http
+		List<Alumno> alumnos = as.getAll();
+		ResponseEntity<List<Alumno>> respuesta = null;
+		if(alumnos.isEmpty()){
+			respuesta = new ResponseEntity<List<Alumno>>(HttpStatus.NO_CONTENT); 
+		}else{
+			respuesta = new ResponseEntity<List<Alumno>>(alumnos, HttpStatus.OK);
+		}
 		
-		return null;
+		return respuesta;
 	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Alumno> getById(@PathVariable("id") int id){
+		Alumno alumno = as.getById(id);
+		ResponseEntity<Alumno> respuesta = null;
+		if(alumno == null){
+			respuesta = new ResponseEntity<Alumno>(HttpStatus.NOT_FOUND); 
+		}else{
+			respuesta = new ResponseEntity<Alumno>(alumno,HttpStatus.OK); 
+		}
+		
+		return respuesta;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> create(Alumno alumno){
+		Alumno alum = as.create(alumno);
+		ResponseEntity<Void> respuesta = null;
+		if(alum.getCodigo()>-1){
+			respuesta = new ResponseEntity<Void>(HttpStatus.CREATED);
+		}else{
+			respuesta = new ResponseEntity<Void>(HttpStatus.CONFLICT);
+		}
+		
+		return respuesta;
+		
+	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable("id") int id){
+		ResponseEntity<Void> respuesta = null;
+		if(as.getById(id)!=null){
+			as.delete(id);
+			respuesta = new ResponseEntity<Void>(HttpStatus.NO_CONTENT); 
+		}else{
+			respuesta = new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+		}
+		
+		return respuesta;
+		
+	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.PUT) //se pone la id xq la update afecta a un recurso
+	public ResponseEntity<Alumno> update(@PathVariable("id") int id,@RequestBody Alumno alumno){ 
+		ResponseEntity<Alumno> respuesta = null;
+		if(as.getById(id)!=null){
+			alumno.setCodigo(id);
+			as.update(alumno);
+			respuesta = new ResponseEntity<Alumno>(alumno,HttpStatus.OK);
+		}else{
+			respuesta = new ResponseEntity<Alumno>(HttpStatus.NOT_FOUND);
+		}
+		
+		return respuesta;
+		
+	}
+	
+	
 }
